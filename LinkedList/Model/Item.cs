c6a1385f824c94f60ca73aa7a073fd69806293f2
Yehor <ametionqq@gmail.com>
2022-05-27 17:34:02 +0200

    Create my own Linked List (single)

---
+++ LinkedList/Model/Item.cs
@@ -0,0 +1,23 @@
+﻿namespace LinkedList.Model;
+
+public class Item<T>
+{
+    private T data = default(T);
+
+    public Item<T> Next { get; set; }
+    public T Data
+    {
+        get { return data; }
+        set
+        {
+            if (value != null)
+                data = value;
+            else
+                throw new ArgumentNullException(nameof(value));
+        }
+    }
+
+    public Item(T data) => Data = data;
+
+    public override string ToString() => Data.ToString();
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using LinkedList.Model;
+
+class Program
+{
+    static void Main()
+    {
+        var list = new MyLinkedList<int>();
+        
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        list.Add(5);
+
+        foreach (var item in list)
+            Console.WriteLine(item);
+
+        list.Delete(3);
+
+        Console.WriteLine("-------------------");
+        
+        foreach (var item in list)
+            Console.WriteLine(item);
+        
+        list.InsertAfter(2, 15);
+        
+        Console.WriteLine("-------------------");
+        
+        foreach (var item in list)
+            Console.WriteLine(item);
+        
+        Console.ReadLine();
+    }
+}
